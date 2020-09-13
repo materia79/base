be7a1df8b16961d33a6766e1f896442d34eeb4dd
Simon <simon@istaria.de>
2020-09-13 15:42:59 +0200

    cleanup

--- packages/clientLogger/index.js
+++
@@ -1,9 +0,0 @@
-const esc = `\x1b`;
-const sgr = (...args) => `${esc}[${args.join(';')}m`;
-const sgrRgbFg = (...args) => sgr(`38;2;${args.join(';')}`);
-const sgrRgbBg = (...args) => sgr(`48;2;${args.join(';')}`);
-const normal = sgrRgbFg(192, 192, 192) + sgrRgbBg(0, 0, 0);
-
-mp.events.add("log", (player, text) => {
-  console.log(normal.concat("[", sgrRgbBg(0, 0, 0), player.name, sgrRgbFg(192, 192, 192), "(", player.id, ")", normal, "] ", text, normal));
-});
