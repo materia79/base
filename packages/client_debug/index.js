bcfa502dd16c2e8c960701f5e093c1b70fd5e4ef
Simon <simon@istaria.de>
2020-11-27 19:49:02 +0100

    +

--- packages/client_debug/index.js
+++ packages/client_debug/index.js
@@ -3,7 +3,9 @@ const sgr = (...args) => `${esc}[${args.join(';')}m`;
 const sgrRgbFg = (...args) => sgr(`38;2;${args.join(';')}`);
 const sgrRgbBg = (...args) => sgr(`48;2;${args.join(';')}`);
 const normal = sgrRgbFg(192, 192, 192) + sgrRgbBg(0, 0, 0);
+const bgBlack = sgrRgbBg(0, 0, 0);
+const fgGrey = sgrRgbFg(192, 192, 192);
 
 mp.events.add("log", (player, text) => {
-  console.log(normal.concat("[", sgrRgbBg(0, 0, 0), player.name, sgrRgbFg(192, 192, 192), "(", player.id, ")", normal, "] ", text, normal));
+  console.log(normal.concat("[", bgBlack, player.name, fgGrey, "(", player.id, ")", normal, "] ", text, normal));
 });
