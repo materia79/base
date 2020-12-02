1ce20834a051a7ce8c7dcf349a02191869bcd1de
Simon <simon@istaria.de>
2020-12-02 22:16:20 +0100

    r command error stack

--- packages/server_console/commands/r.js
+++ packages/server_console/commands/r.js
@@ -3,9 +3,15 @@ module.exports = {
     args.shift();
     if (args.length < 1) return "Usage: `r <serverside code>`";
     let commandLine = args.join(" ");
-    let f = new Function(commandLine);
-    let result = f();
-    return "Return value: ".concat(result);
+    
+    try {
+      let f = new Function(commandLine);
+      let result = f();
+      return "Return value: ".concat(result);
+    } catch (error) {
+      console.log("[Run] Error in serverside script: " + error.stack);
+      return;
+    }
   },
   help: `    r <server side code>      - run serverside code\n`
 }
