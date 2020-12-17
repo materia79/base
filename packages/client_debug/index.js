81871ab49bf20148b181edd03a1602da6ddfce14
Simon <simon@istaria.de>
2020-12-17 14:31:56 +0100

    fixed console foreground color

--- packages/client_debug/index.js
+++ packages/client_debug/index.js
@@ -2,10 +2,11 @@ const esc = `\x1b`;
 const sgr = (...args) => `${esc}[${args.join(';')}m`;
 const sgrRgbFg = (...args) => sgr(`38;2;${args.join(';')}`);
 const sgrRgbBg = (...args) => sgr(`48;2;${args.join(';')}`);
-const normal = sgrRgbFg(192, 192, 192) + sgrRgbBg(0, 0, 0);
+const normal = sgr(0); //sgrRgbFg(192, 192, 192) + sgrRgbBg(0, 0, 0);
 const bgBlack = sgrRgbBg(0, 0, 0);
 const fgGrey = sgrRgbFg(192, 192, 192);
+const clientDebugColor = sgrRgbFg(192, 64, 64);
 
 mp.events.add("log", (player, text) => {
-  console.log(normal.concat("[", bgBlack, player.name, fgGrey, "(", player.id, ")", normal, "] ", text, normal));
+  console.log(normal.concat("[", bgBlack, player.name, fgGrey, "(", player.id, ")", normal, "] ", clientDebugColor, text, normal));
 });
