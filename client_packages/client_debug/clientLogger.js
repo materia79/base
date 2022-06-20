8f94a88f873e3fa504afd059ced6eaef0e4c948a
Simon <simon@istaria.de>
2022-06-20 22:57:43 +0200

    optimized performance a little

--- client_packages/client_debug/clientLogger.js
+++ client_packages/client_debug/clientLogger.js
@@ -3,15 +3,14 @@
   mp.log = function () { setTimeout(mp.log, 10, Array.from(arguments)); };
   while (!mp.events.EventMerger) await mp.game.waitAsync(0);
 
-  const clientlog = new mp.events.EventMerger(function () {
+  // event merger
+  const clientlog = new mp.events.EventMerger(function (string) {
     try {
-      if (this.queue.dup > 1) {  // multiple events
-        mp.events.callRemote('log', this.queue.args.join(" ") + " [ " + this.queue.dup + " x DUP ]");
-      } else {                   // only one event
-        mp.events.callRemote('log', this.queue.args.join(" "));
-      }
+      if (this.queue.dup > 1) mp.events.callRemote('log', string + " [ " + this.queue.dup + " x DUP ]");    // multiple events
+      else mp.events.callRemote('log', string);                                                             // only one event
     } catch (error) { mp.events.callRemote("log", "[log.handler] " + error.stack); }
   }, 250, 3000);
-  mp.log = clientlog.add;
+
+  mp.log = function () { clientlog.add(([...arguments]).join(" ")); };
   mp.events.add("log", mp.log);
 })();
