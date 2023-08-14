3e03e5652c42cf9b49ac8a82a0fb1fa7494f37e9
Simon <simon@istaria.de>
2023-08-14 14:29:54 +0200

    support for async serverside run code

--- packages/server_console/commands/r.js
+++ packages/server_console/commands/r.js
@@ -1,17 +1,44 @@
+const
+  getAsyncScript = (script) => {
+    return `
+(async () => {
+  try {
+    let _f = async function() { ` + script + ` };
+    let _res = await _f();
+    await wait(50);
+    console.log("[run(async)] Result: ".concat(typeof _res == "object" ? JSON.stringify(_res) : _res));
+  } catch (error) {
+    await wait(50);
+    console.log("[run(async)] Error in clientside script: " + error);
+  }
+})();`;
+  },
+  getScript = (script) => {
+    return `
+(() => {
+  try {
+    let _f=function() { ` + script + ` };
+    let _res=_f();
+    console.log("[run] Result: ".concat(typeof _res == "object" ? JSON.stringify(_res) : _res));
+  } catch (error) {
+    console.log("[run] Error in script: " + error);
+  }
+})();`;
+  };
+
 module.exports = {
   cmd: (args) => {
     args.shift();
     if (args.length < 1) return "Usage: `r <serverside code>`";
-    let commandLine = args.join(" ");
-    
-    try {
-      let f = new Function(commandLine);
-      let result = f();
-      return typeof result != "undefined" ? "[Run] return value: ".concat(result) : undefined;
-    } catch (error) {
-      console.log("[Run] " + error);
-      return;
+    const
+      code = args.join(" "),
+      isAsync = code.indexOf("await") >= 0;
+
+    if (isAsync) {
+      eval(getAsyncScript(code));
+    } else {
+      eval(getScript(code));
     }
   },
-  help: `    r <server side code>      - run serverside code\n`
+  help: `    r <server side code>      - run serverside code (supports async await)\n`
 }
